9b44ac3457b0abe56045334d30760696e8794a13
Valera <valerakaktotak@gmail.com>
2021-12-01 11:46:03 +0200

    added layouts/footer.php and include it in index.php

---
+++ layouts/footer.php
@@ -0,0 +1,10 @@
+<footer class="p-3 bg-dark text-white">
+    <div class="container">
+        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+            <div>
+                <a style="margin: 10px 0; text-decoration: none; display: inline-block;" href="https://www.facebook.com/">Facebook</a>
+                <p>Copyright © 2021</p>
+            </div>
+        </div>
+    </div>
+</footer>
