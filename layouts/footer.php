9073c004ef6affafd88cc234c10342406bf2d8f4
raspberry-mg <92121426+raspberry-mg@users.noreply.github.com>
2021-12-01 12:20:13 +0200

    Add menu in footer (#5)
    Co-authored-by: ArtemKhabarov21 <artem.khabarov@outlook.com>

--- layouts/footer.php
+++ layouts/footer.php
@@ -1,5 +1,15 @@
 <footer class="p-3 bg-dark text-white">
     <div class="container">
+         <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                <li><a href="#" class="nav-link px-2 text-secondary">Home</a>
+                </li>
+                <li><a href="#" class="nav-link px-2 text-white">Features</a>
+                </li>
+                <li><a href="#" class="nav-link px-2 text-white">Pricing</a>
+                </li>
+                <li><a href="#" class="nav-link px-2 text-white">FAQs</a></li>
+                <li><a href="#" class="nav-link px-2 text-white">About</a></li>
+            </ul>
         <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
             <div>
                 <a style="margin: 10px 0; text-decoration: none; display: inline-block;" href="https://www.facebook.com/">Facebook</a>
